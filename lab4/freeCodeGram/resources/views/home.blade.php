9459b54c48ac8c1514b4941486ff3df817e66eb2
roksonn <achimdei@gmail.com>
2022-01-27 23:33:22 +0200

    Laravel

---
+++ lab4/freeCodeGram/resources/views/home.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://pbs.twimg.com/profile_images/1276770212927410176/qTgTIejk_400x400.jpg" class="rounded-circle" style="height: 250px;">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>freecodecamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>437</strong> posts</div>
+                <div>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</div>
+                <div class="pr-5"><strong>89.2k</strong> followers</div>
+                <div>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp</div>
+                <div class="pr-5"><strong>342</strong> following</div>
+            </div>
+            <div class="pt-4"><strong>freeCodeCamp.org</strong></div>
+            <div>.We're a global community of millions of people learning to code together.<br>
+                LearnToCodeRPG: https://www.freecodecamp.org/news/learn-to-code-rpg/</div>
+            <div><a href="#">www.freecodecamp.org</a></div>
+        </div>
+    </div>
+
+    <div class="row pt-5">
+        <div class="col-4">
+            <img src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://palmiye.ro/wp-content/uploads/2021/12/Cutie-praline_300gr_1-450x450.png" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://redmobile.ro/wp-content/uploads/2021/04/Husa-Bumper-iPhone-12-12-Mini-12-Pro-Max-450x450-1.png" class="w-100">
+        </div>
+    </div>
+
+</div>
+@endsection
